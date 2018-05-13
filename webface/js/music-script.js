2a1ef4e5a58c620cee4f4bd665513eb6488f10c3
Ashwin Abraham <ashwinabraham96@gmail.com>
2018-05-13 15:50:43 +0530

    Front end done

---
+++ webface/js/music-script.js
@@ -0,0 +1,15 @@
+// Enviroment Selection
+(function () {
+  document.querySelector('#enviromentSelection').addEventListener('click', function () {
+    this.classList.toggle('_active');
+  });
+})();
+
+// Enviroment Music
+(function () {})();
+
+// Ambiences
+(function () {})();
+
+// Player
+(function () {})();
