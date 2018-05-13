e18a759a639dea9e4e90bc4bb16b8f2a7bf16294
Ashwin Abraham <ashwinabraham96@gmail.com>
2018-05-14 01:51:04 +0530

    Music Done

--- index.js
+++ index.js
@@ -3,27 +3,176 @@ const url = require('url');
 const fs = require('fs');
 const mime = require('mime');
 const socketio = require('socket.io');
+const uuid = require('uuid');
 
-const server = http.createServer(function (req, res) {
-  let URL = url.parse(req.url);
-  if (URL.path[URL.path.length - 1] === "/")  URL.path += "index.html";
-  fs.readFile("./webface" + URL.path, function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      res.end("Server Error");
+function addSpecial (path) {
+  var id = uuid.v4();
+  specials.push({
+    id: id,
+    path: path
+  });
+  return id;
+};
+function validateSoundJSON (filename) {
+  var soundjsonprops = {
+    "absolutestart": val => typeof val === "number" && val >= 0,
+    "loopstart": val => typeof val === "number",
+    "loopend": val => typeof val === "number",
+    "volume": val => typeof val === "number" && val >= 0 && val <= 1
+  };
+  var json = fs.readFileSync(filename, 'utf-8');
+  try {
+    var json = JSON.parse(json);
+    Object.keys(soundjsonprops).forEach(p => {
+      if (soundjsonprops[p](json[p]) === false) {
+        return false;
+      }
+    });
+    if (json.loopstart > json.loopend) {
+      return false; // FIXME: Check for negative values too
     }
-    else {
-      res.writeHead(200, {
-        'Content-Length': Buffer.byteLength(data),
-        'Content-Type': mime.getType("./webface" + URL.path)
+  }
+  catch (e) {
+    return false;
+  }
+  return true;
+};
+function listSounds () {
+  return fs.readdirSync('./enviroments').map(n => {
+    if (fs.statSync('./enviroments/' + n).isDirectory()) {
+      let contents = fs.readdirSync('./enviroments/' + n);
+      let mp3s = contents.filter(m => {
+        if (fs.statSync('./enviroments/' + n + '/' + m).isFile() && mime.getType('./enviroments/' + n + '/' + m).indexOf('audio') > -1) {
+          return true;
+        }
       });
-      res.end(data);
+      let jsons = contents.filter(m => {
+        if (fs.statSync('./enviroments/' + n + '/' + m).isFile() && mime.getType('./enviroments/' + n + '/' + m) === "application/json") {
+          return true;
+        }
+      });
+      let validsounds = mp3s.filter(function (m) {
+        var name = /([a-zA-Z0-9 -]+)\.mp3/.exec(m);
+        if (jsons.indexOf(name[1] + ".json") > -1) {
+          // check json
+          return validateSoundJSON('./enviroments/' + n + '/' + name[1] + '.json');
+        }
+        else {
+          return false;
+        }
+      }).map(m => /([a-zA-Z0-9 -]+)\.mp3/.exec(m)[1]);
+      console.log(n, validsounds);
+      return {
+        name: n,
+        sounds: validsounds
+      }
+    }
+  }).filter(r => r);
+};
+function listAmbiences () {
+  let contents = fs.readdirSync('./ambiences');
+  let mp3s = contents.filter(m => {
+    if (fs.statSync('./ambiences/' + m).isFile() && mime.getType('./ambiences/' + m).indexOf('audio') > -1) {
+      return true;
     }
   });
+  let jsons = contents.filter(m => {
+    if (fs.statSync('./ambiences/' + m).isFile() && mime.getType('./ambiences/' + m) === "application/json") {
+      return true;
+    }
+  });
+  return mp3s.filter(function (m) {
+    var name = /([a-zA-Z0-9 -]+)\.mp3/.exec(m);
+    if (jsons.indexOf(name[1] + ".json") > -1) {
+      // check json
+      return validateSoundJSON('./ambiences/' + name[1] + '.json');
+    }
+    else {
+      return false;
+    }
+  }).map(m => /([a-zA-Z0-9 -]+)\.mp3/.exec(m)[1]);
+};
+
+let specials = [];
+const server = http.createServer(function (req, res) {
+  let URL = url.parse(req.url);
+  if (URL.query) {
+    let modpath = URL.query.split("&").reduce(function (ass, val) {
+      let m = /([a-zA-Z0-9-]+)=([a-zA-Z0-9-]+)/.exec(val);
+      ass[m[1]] = m[2];
+      return ass;
+    }, {}).s;
+    if (modpath && specials.find(val => val.id === modpath) !== undefined) {
+      var index = specials.find(val => val.id === modpath);
+      fs.readFile(index.path, function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          res.end("Server Error");
+        }
+        else {
+          res.writeHead(200, {
+            'Content-Length': Buffer.byteLength(data),
+            'Content-Type': mime.getType(index.path)
+          });
+          res.end(data);
+        }
+      });
+    }
+  }
+  else {
+    if (URL.path[URL.path.length - 1] === "/")  URL.path += "index.html";
+    fs.readFile("./webface" + URL.path, function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        res.end("Server Error");
+      }
+      else {
+        res.writeHead(200, {
+          'Content-Length': Buffer.byteLength(data),
+          'Content-Type': mime.getType("./webface" + URL.path)
+        });
+        res.end(data);
+      }
+    });
+  }
 });
 let io = socketio(server);
 io.on('connection', function (socket) {
-
+  let socketspecials = [];
+  socket.emit('sounds', listSounds());
+  socket.emit('ambiences', listAmbiences());
+  socket.on('sound', function (data) {
+    let folder = data.split("::::")[0];
+    let name = data.split("::::")[1];
+    let id = addSpecial('./enviroments/' + folder + '/' + name + '.mp3'); // FIXME: Bottle neck for different types of sound files (change mp3)
+    socketspecials.push(id);
+    setTimeout(() => {
+      socket.emit('sound', {
+        folder: folder,
+        name: name,
+        json: JSON.parse(fs.readFileSync('./enviroments/' + folder + '/' + name + '.json', 'utf-8')),
+        sound: id
+      });
+    }, 100);
+  });
+  socket.on('ambient', function (data) {
+    let name = data;
+    let id = addSpecial('./ambiences/' + data + '.mp3');
+    socketspecials.push(id);
+    setTimeout(() => {
+      socket.emit('ambient', {
+        name: name,
+        json: JSON.parse(fs.readFileSync('./ambiences/' + data + '.json', 'utf-8')),
+        sound: id
+      });
+    }, 100);
+  });
+  socket.on('disconnected', function () {
+    socketspecials.forEach(id => {
+      let index = specials.find(val => val.id === id);
+      if (index !== undefined) specials.splice(index, 1);
+    });
+  });
 });
 server.listen(4000, function (err) {
   if (err) throw err
--- webface/js/music-script.js
+++ webface/js/music-script.js
@@ -1,15 +1,146 @@
-// Enviroment Selection
+var socket = io();
+
+// Enviroment
 (function () {
+  let envdata = [];
+
+  function setEnviromentData (data) {
+    envdata = data;
+    let txt = "";
+    data.forEach(n => {
+      txt += `<div class="item" data-name="${n.name}">${n.name}</div>`;
+    });
+    document.querySelector('#enviromentSelection_list').innerHTML = txt;
+    document.querySelectorAll('#enviromentSelection_list > .item').forEach(p => {
+       p.addEventListener('click', function () {
+         setEnviromentSounds(this.getAttribute('data-name'));
+       });
+     });
+  };
+  setEnviromentSounds = function (enviromentname) {
+    envdata.forEach(n => {
+      if (n.name === enviromentname) {
+        document.querySelector('#enviromentSelection_title').innerHTML = enviromentname;
+        let txt = "";
+        n.sounds.forEach(m => {
+          txt += `<div class="item" data-sound="${enviromentname}::::${m}">${m}</div>`;
+        });
+        if (n.sounds.length === 0) {
+          txt = `<div class="nosounds">No Enviroment Sounds Found</div>`;
+        }
+        document.querySelector('#enviromentSounds').innerHTML = txt;
+        document.querySelectorAll('#enviromentSounds > .item').forEach(p => {
+          p.addEventListener('click', function () {
+            Player.playSound(this.getAttribute('data-sound'));
+          });
+        });
+      }
+    });
+  };
+
   document.querySelector('#enviromentSelection').addEventListener('click', function () {
     this.classList.toggle('_active');
   });
+
+  socket.on('sounds', function (data) {
+    setEnviromentData(data);
+    if (data.length > 0) {
+      setEnviromentSounds(data[0].name);
+    }
+  });
 })();
 
-// Enviroment Music
-(function () {})();
-
 // Ambiences
-(function () {})();
+(function () {
+  socket.on('ambiences', function (data) {
+    let txt = "";
+    data.forEach(p => {
+      txt += `<div class="item" data-sound="${p}">${p}</div>`;
+    });
+    if (data.length === 0) txt += `<div class="nosounds">No Ambiences Available</div>`;
+    document.querySelector('#ambience').innerHTML = txt;
+    document.querySelectorAll('#ambience > .item').forEach(function (ele) {
+      ele.addEventListener('click', function () {
+        console.log(this.getAttribute('data-sound'));
+        Player.playAmbient(this.getAttribute('data-sound'));
+      });
+    });
+  });
+})();
 
 // Player
-(function () {})();
+(function () {
+  Player = {};
+
+  let audio = new Audio;
+  let audioendtime = 0;
+  let audiostarttime = 0;
+  let absolutestarttime = 0;
+
+  function resetAudio () {
+    if (audiostarttime >= 0) {
+      audio.currentTime = audiostarttime;
+    }
+    else {
+      audio.currentTime = (audio.duration + audiostarttime);
+    }
+  };
+
+  Player.playSound = function (value) {
+    socket.emit('sound', value);
+  };
+  Player.playAmbient = function (value) {
+    socket.emit('ambient', value);
+  };
+
+  window.Player = Player;
+
+  document.querySelector('#player_button').addEventListener('click', function () {
+    if (audio.readyState === 4) {
+      if (audio.paused) {
+        audio.play();
+        document.querySelector('#player').classList.add('_playing');
+      }
+      else {
+        audio.pause();
+        document.querySelector('#player').classList.remove('_playing');
+      }
+    }
+  });
+  audio.addEventListener('canplay', function () {
+    this.currentTime = absolutestarttime;
+    this.play();
+  });
+  audio.addEventListener('timeupdate', function (e) {
+    var ms = this.currentTime * 1000;
+    document.querySelector('#player_time').innerHTML = moment.duration(ms).format('hh:mm:ss');
+    // check if reached end
+    if (audioendtime > 0) {
+      if (this.currentTime >= audioendtime) {
+        resetAudio();
+      }
+    }
+  });
+  audio.addEventListener('ended', function () {
+    resetAudio();
+    audio.play();
+  });
+  socket.on('sound', function (data) {
+    audio.src = location.href + '?s=' + data.sound;
+    document.querySelector('#player_current').innerHTML = data.name + " - " + data.folder;
+    document.querySelector('#player').classList.add('_playing');
+    absolutestarttime = data.json.absolutestart / 1000;
+    audiostarttime = data.json.loopstart / 1000;
+    audioendtime = data.json.loopend / 1000;
+    audio.volume = data.json.volume;
+  });
+  socket.on('ambient', function (data) {
+    audio.src = location.href + '?s=' + data.sound;
+    document.querySelector('#player_current').innerHTML = data.name;
+    document.querySelector('#player').classList.add('_playing');
+    absolutestarttime = data.json.absolutestart / 1000;
+    audiostarttime = data.json.loopstart / 1000;
+    audioendtime = data.json.loopend / 1000;
+    audio.volume = data.json.volume;
+  });
+})();
